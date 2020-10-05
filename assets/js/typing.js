0d566a42b5decbac6fa6cf12e5b3cb2c99594459
ASHISH <ashish2829003@gmail.com>
2020-10-05 12:44:28 +0530

    home section updated

---
+++ assets/js/typing.js
@@ -0,0 +1,11 @@
+
+$(function () {
+    $(".typed").typed({
+        strings: ["We are team of talanted digital marketers.", "We are  Web Developers.", "We are Civil Engineers."],
+        typeSpeed: 10,
+        backSpeed: 10,
+        backDelay: 1500,
+        showCursor: true,
+        loop: true
+    });
+});
