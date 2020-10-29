f2872ebfc9e136e52807a25f59ac730cca299a01
ASHISH <ashish2829003@gmail.com>
2020-10-29 16:13:01 +0530

    Homepage background change

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -139,19 +139,19 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
+      var error_msg = "This service is currently disabled!";
       if(data.statusText || data.status) {
-        error_msg += 'Status:';
+        // error_msg += 'Status:';
         if(data.statusText) {
-          error_msg += ' ' + data.statusText;
+          // error_msg += ' ' + data.statusText;
         }
         if(data.status) {
-          error_msg += ' ' + data.status;
+          // error_msg += ' ' + data.status;
         }
         error_msg += '<br>';
       }
       if(data.responseText) {
-        error_msg += data.responseText;
+        // error_msg += data.responseText;
       }
       this_form.find('.loading').slideUp();
       this_form.find('.error-message').slideDown().html(error_msg);
